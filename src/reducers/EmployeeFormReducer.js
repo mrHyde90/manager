4b0187612a0d8a249ce7d58e3c5cc80245609acf
jose alfredo <a00516283@gmail.com>
2017-09-17 14:52:24 -0500

    Edit form finished

--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -1,4 +1,4 @@
-import {EMPLOYEE_UPDATE, EMPLOYEE_CREATE} from '../actions/types';
+import {EMPLOYEE_UPDATE, EMPLOYEE_CREATE, EMPLOYEE_SAVE_SUCCESS} from '../actions/types';
 
 const INITIAL_STATE = {
 	name: "",
@@ -10,6 +10,8 @@ export default (state=INITIAL_STATE, action) => {
 	switch(action.type){
 		case EMPLOYEE_UPDATE:
 			return { ...state, [action.payload.prop]: action.payload.value };
+		case EMPLOYEE_SAVE_SUCCESS:
+			return INITIAL_STATE;
 		case EMPLOYEE_CREATE:
 			return INITIAL_STATE;
 		default:
